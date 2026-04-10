dce9881b83522009e22ea195541e480f6578fbbc
AristosDragatis <dragatis.aris@gmail.com>
2026-04-10 20:34:32 +0300

    add: authController for registering users

--- src/main/java/com/example/financetracker/controller/AuthController.java
+++ src/main/java/com/example/financetracker/controller/AuthController.java
@@ -1,4 +1,27 @@
 package com.example.financetracker.controller;
 
+import com.example.financetracker.service.UserService;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService){
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String>  registerUser(@RequestParam("name") String name, @RequestParam("email") String email, @RequestParam("password") String password){
+        userService.registerUser(name, email, password);
+        return ResponseEntity.ok("User registered successfully!");
+    }
+
+
 }
