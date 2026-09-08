2f19b5bbc8fb5a2f4b282969b7bddc165bcf6a80
AristosDragatis <dragatis.aris@gmail.com>
2026-09-08 21:13:17 +0300

    todo: fill Account entity

---
+++ backend/src/main/java/com/example/financetracker/domain/Account.java
@@ -0,0 +1,15 @@
+package com.example.financetracker.domain;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+// TODO: fix this later
+@Entity
+public class Account {
+    @Id
+    private Long id;
+
+
+}
